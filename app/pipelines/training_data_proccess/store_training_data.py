eae0d311bd854b24f0545b87e05a8d2efa15fb3f
sahar mirjavadi <s.mirjavadi@vie-bank.com>
2023-08-14 14:17:43 +0330

    remove extra proccess and store user input directly in the database

--- app/pipelines/training_data_proccess/store_training_data.py
+++ app/pipelines/training_data_proccess/store_training_data.py
@@ -6,18 +6,7 @@ from datetime import datetime
 from ...api.preparation_models import naive_bayes
 
 
-
 def reinforcement_model() -> None:
     print('STORE TASK', datetime.now())
     db = SessionLocal()
-    file_path = 'user_input_log.txt'
-    if os.path.exists(file_path):
-        with open(file_path, 'r', encoding='utf-8') as file:
-            for line in file:
-                sentence, action, model_id = line.strip().split(' - ')
-                base_crud = BaseCRUD(TrainingData)
-                training_data_obj = {'sentence': sentence,
-                                     'predicted_action': action, 'model_id': model_id}
-                base_crud.create(db=db, **training_data_obj)
-        os.remove(file_path)
-        naive_bayes(db)
+    naive_bayes(db)
